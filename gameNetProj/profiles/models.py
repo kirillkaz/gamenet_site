fbb841c829b4500e0cd8595c402788b680097f82
kirillkaz <kirillkazazaev@gmail.com>
2023-08-03 00:31:04 +1000

    user model demo

--- gameNetProj/profiles/models.py
+++ gameNetProj/profiles/models.py
@@ -1,6 +1,33 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, PermissionsMixin
+from datetime import date
 # Create your models here.
 
-class GameNetUser(AbstractBaseUser):
-    pass
+class User(AbstractBaseUser):
+    login = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=14)
+    last_login = None
+
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    
+    gamelist_id = models.IntegerField()
+    avatar_id = models.IntegerField()
+
+    is_superuser = False
+    is_staff = False
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.position == 'Разработчик БД'
+
+    @property
+    def is_superuser(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.position == 'Разработчик БД'
