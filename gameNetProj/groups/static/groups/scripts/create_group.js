a2485832a7e0e4935c501819775a56b630888f0f
kirillkaz <kirillkazazaev@gmail.com>
2023-08-29 05:57:12 +0000

    started frontend for groups page

--- gameNetProj/groups/static/groups/scripts/create_group.js
+++ gameNetProj/groups/static/groups/scripts/create_group.js
@@ -1,11 +1,15 @@
 const showCreateWindow = () => {
     $.ajax({
         type: "get",
-        url: "http://0.0.0.0/groups/ajax/groupcreation",
+        url: "http://37.139.33.69/groups/ajax/groupcreation",
         success: (data) => {
             console.log(data)
             $('#group_creation').empty();
             $('#group_creation').append(data);
         }
     });
+}
+
+const hideCreateWindow = () => {
+    $('#group_creation').empty()
 }
