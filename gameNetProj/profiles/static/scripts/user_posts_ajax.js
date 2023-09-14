c08d10098e3876de9b15882536885b99dd10140e
kirillkaz <kirillkazazaev@gmail.com>
2023-09-14 21:24:42 +1000

    added empty post error

--- gameNetProj/profiles/static/scripts/user_posts_ajax.js
+++ gameNetProj/profiles/static/scripts/user_posts_ajax.js
@@ -1,17 +1,25 @@
 const CreatePost = (u_login, u_password) => {
     let form_data = new FormData($('#create-post-form').get(0))
+    let textarea_value = document.getElementsByClassName('post-input')[0].value
 
-    $.ajax({
-        type: "post",
-        url: `http://0.0.0.0/activities/users/posts`,
-        dataType: 'json',
-        contentType: false,
-        processData: false,
-        username: u_login,
-        password: u_password,
-        data: form_data,
-        success: (data) => {
-            console.log('Успех!')
-        }
-    });
+    if (textarea_value == ''){
+        $('#empty-post-error').empty();
+        $('#empty-post-error').append('<div class="error-text">К сожалению вы не можете создать пустой пост.</div>');
+    }
+
+    else{
+        $.ajax({
+            type: "post",
+            url: `http://0.0.0.0/activities/users/posts`,
+            dataType: 'json',
+            contentType: false,
+            processData: false,
+            username: u_login,
+            password: u_password,
+            data: form_data,
+            success: (data) => {
+                console.log('Успех!')
+            }
+        });
+    }
 }
