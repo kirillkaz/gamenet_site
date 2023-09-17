0832869f96a0ee00e57948836f70241f0d464c6c
kirillkaz <kirillkazazaev@gmail.com>
2023-09-17 13:25:14 +1000

    get_ajax_wrapper updated

--- gameNetProj/tools/ajax_wrapper.py
+++ gameNetProj/tools/ajax_wrapper.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseBadRequest
 
-def get_ajax_wrapper(request, url, context):
+
+
+def get_ajax_wrapper(request, url='', context={}, return_type='html'):
+    """
+    Return path of page with ajax or json data with ajax.
+
+    url = path to html file. (defauld='')
+
+    context = data for django template engine
+
+    return_type - can be 'html' or 'json'. (default = 'html')
+    """
     is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
 
     if is_ajax:
         if request.method == 'GET':
-            return render(request, url, context)
+            if return_type == 'html':
+                return render(request, url, context)
+            
+            elif return_type == 'json':
+                return JsonResponse({'context': context})
         else:
             return JsonResponse({'req_error': 'invalid request'}, status=400)
         
